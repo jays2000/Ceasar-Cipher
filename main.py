2272021cbbe3f9dc1f70ef219df2a0fd8852ce4f
jays2000 <84200424+jays2000@users.noreply.github.com>
2022-02-19 11:32:57 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+
+arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w","x","y","z"]
+key = 0
+
+def writeToFile(string):
+    file = open("output.txt", "w")
+    file.write(string)
+    file.close()
+
+def encrypt(line):
+    encryptedString = ""
+    global key
+    key = key % 26
+
+    for i in range(0, len(line)):
+        shift = arr.index(line[i]) + key
+        encryptedString = encryptedString + arr[shift]
+
+    writeToFile(encryptedString)
+
+
+def readFile(file):
+    try:
+        with open(file) as f:
+            line = f.read()
+            line = line.lower()
+            encrypt(line)
+
+    except FileNotFoundError:
+        print("Could not open file")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("Pleas enter correct arguments")
+    else:
+        if sys.argv[1] == '-e':
+            try:
+                key = int(sys.argv[3])
+                readFile(sys.argv[2])
+            except ValueError:
+                print("Illegal entry")
